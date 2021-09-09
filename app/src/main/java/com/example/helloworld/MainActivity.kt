acbb19a2d9689d274657cb0fd5e29abd416e9054
thuc luong <dungluong67@gmail.com>
2021-09-09 09:49:05 -0400

    added find view by ID to button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener({v, ->(v as Button).text = "You Clicked the Button"})
+
     }
 }
